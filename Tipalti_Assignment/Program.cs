f2912416cb6800f4f9de3e344db6a6220fd4ad0a
Amir Leitner <amirle93@gmail.com>
2023-09-03 18:34:11 +0300

    Add files via upload

---
+++ Tipalti_Assignment/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Tipalti_Assignment
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Persons
+            Person AlanTuring = new Person
+            {
+                FullName = new Name { FirstName = "Alan", LastName = "Turing" },
+                Address = new Address { Street = "Bletchley Park", City = "NY" }
+            };
+
+            Person JoanClarke = new Person
+            {
+                FullName = new Name { FirstName = "Joan", LastName = "Clark" },
+                Address = new Address { Street = "Bletchley Park", City = "NY" }
+            };
+
+            Person GraceHopper = new Person
+            {
+                FullName = new Name { FirstName = "Grace", LastName = "Hopper" },
+                Address = new Address { Street = "Bronx", City = "New York" }
+            };
+
+            Person SecondAlanTuring = new Person
+            {
+                FullName = new Name { FirstName = "Alan", LastName = "Turing" },
+                Address = new Address { Street = "Hirtut St", City = "Cambridge" }
+            };
+
+            Person BabyShark = new Person
+            {
+                FullName = new Name { FirstName = "Joan", LastName = "Clark" },
+                Address = new Address { Street = "123", City = "London" }
+            };
+
+            RelationUtility utility = new RelationUtility();
+            utility.Init(new[] { AlanTuring, JoanClarke, GraceHopper, SecondAlanTuring, BabyShark });
+
+            // Test 1
+            Test(1, utility, AlanTuring, JoanClarke, 1);            //same address
+
+            // Test 2
+            Test(2, utility, AlanTuring, GraceHopper, -1);             //no connection
+
+            // Test 3
+            Test(3, utility, JoanClarke, GraceHopper, -1);             //no connection
+
+            // Test 4
+            Test(4, utility, AlanTuring, BabyShark, 2);                 // AlanTuring is related to JoanClarke that related to BabyShark
+            
+            // Test 5
+            Test(5, utility, AlanTuring, AlanTuring, 0);                //same person
+
+            // Test 6
+            Test(6, utility, AlanTuring, SecondAlanTuring, 1);          //same name
+            Console.ReadLine();
+        }
+
+        private static void Test(int testNumber, RelationUtility utility, Person personA, Person personB, int expected)
+        {
+            int relationLevel = utility.FindMinRelationLevel(personA, personB);
+            Console.WriteLine($"Test {testNumber}: Expected: {expected}, Got: {relationLevel}");
+            
+        }
+    }
+
+
+}
---
+++ Tipalti_Assignment/RelationUtility.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tipalti_Assignment
+{
+    public class Person
+    {
+        public Name FullName { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Name
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+
+    public class RelationUtility
+    {
+        private List<Person> people;
+
+        public void Init(Person[] persons)
+        {
+            this.people = persons.ToList();
+        }
+
+        public int FindMinRelationLevel(Person personA, Person personB)
+        {
+            if (ArePersonsSame(personA, personB)) return 0;
+
+            var visited = new HashSet<Person>();
+            var queue = new Queue<Tuple<Person, int>>();            //Each element is a tuple containing two values: person and the relation level of that person from the starting person.
+            queue.Enqueue(new Tuple<Person, int>(personA, 0));
+
+            while (queue.Count > 0)             //BFS for finding the minimum relation level between two people
+            {
+                var current = queue.Dequeue();
+                var currentPerson = current.Item1;
+                var distance = current.Item2;
+
+                if (ArePersonsSame(currentPerson, personB))
+                {
+                    return distance;
+                }
+
+                visited.Add(currentPerson);
+
+                // Find people who are directly related to the current person.
+                var directlyRelated = people.Where(p => !visited.Contains(p) && AreDirectlyRelated(currentPerson, p));
+                foreach (var person in directlyRelated)
+                {
+                    queue.Enqueue(new Tuple<Person, int>(person, distance + 1));
+                }
+            }
+
+            return -1;          // No connection found.
+        }
+
+        private bool AreDirectlyRelated(Person personA, Person personB)
+        {
+            return (personA.FullName.FirstName == personB.FullName.FirstName && personA.FullName.LastName == personB.FullName.LastName) ||
+                   (personA.Address.Street == personB.Address.Street && personA.Address.City == personB.Address.City);
+        }
+
+        private bool ArePersonsSame(Person personA, Person personB)
+        {
+            return personA.FullName.FirstName == personB.FullName.FirstName &&
+                   personA.FullName.LastName == personB.FullName.LastName &&
+                   personA.Address.Street == personB.Address.Street &&
+                   personA.Address.City == personB.Address.City;
+        }
+    }
+}
